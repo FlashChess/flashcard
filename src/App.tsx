e51ede0b1c5d93f4159adb666f0b082ce141bcee
Amir <amir.maksyutov@outlook.com>
2022-06-13 14:57:06 -0700

    idea

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Flashcard from "./Flashcard";
+import Homepage from "./Homepage";
+
+export default function App(){
+    // Read URL variables 
+
+    // if (there are the variables){
+    //    return (<Flashcard(...variables) />); 
+    // }
+    // else {
+    //    return (<Homepage />);
+    // }
+
+    // temporary
+    return (
+        <div>App</div>
+    );
+}
