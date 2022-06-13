1b212bca03b19d3e6e2285458bf8673b3be67ba7
Amir <amir.maksyutov@outlook.com>
2022-06-13 16:10:36 -0700

    Need of converter for pgn

--- src/Flashcard.tsx
+++ src/Flashcard.tsx
@@ -1,4 +1,24 @@
+import React, { useState, useRef, useEffect } from "react";
+import Chessground from "./Chessground";
+
+
 export default function Flashcard(title: string, description: string, plannedPGN: string, move: number, turn: string, orientation: string) {
+    const ArrPlannedPGN = useRef<string[]>([]);
+    const initialPGN = useRef<string>();
+    const initialFEN = useRef<string>(); 
+    const point = useRef<number>(0);
+
+    useEffect(() => {
+        if (turn == "white"){
+          point.current = (move - 1) * 2;
+        }
+        else {
+          point.current = (move - 1) * 2 + 1; 
+        }
+    
+        ArrPlannedPGN.current = [];
+      }, []);
+
     return (
         <div>Flashcard</div>
     );
