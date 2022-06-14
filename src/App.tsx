f264bcdddd4f35953ef23dd452e8da250f7c6d8f
Amir <amir.maksyutov@outlook.com>
2022-06-14 12:11:06 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,13 @@ import Homepage from "./Homepage";
 export default function App(){
     // Read URL variables 
     const [searchParams, setSearchParams] = useSearchParams();
-    const plannedPGN = searchParams.get("pgn");
-    const move = searchParams.get("move");
-    const turn = searchParams.get("turn");
-    const orientation = searchParams.get("orientation");
+
     const title = searchParams.get("title");
     const description = "" + searchParams.get("description");
+    const plannedPGN = searchParams.get("pgn");
+    const move = searchParams.get("move");
+    var turn : "white" | "black" = (searchParams.get("turn") === "black") ? "black" : "white";
+    var orientation : "white" | "black" = (searchParams.get("orientation") === "black") ? "black" : "white";
 
     if (title !== null && plannedPGN !== null && move !== null && turn !== null && orientation !== null){
         return (Flashcard(title, description, plannedPGN, parseInt(move), turn, orientation));
