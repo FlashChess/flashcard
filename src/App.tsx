a4dc333c0f50662720a09e892fecd4f88c71a439
Amir <amir.maksyutov@outlook.com>
2022-06-29 20:05:42 -0700

    Description is removed and Homepage is finished

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,13 @@ export default function App(){
     const [searchParams, setSearchParams] = useSearchParams();
 
     const title = searchParams.get("title");
-    const description = "" + searchParams.get("description");
     const plannedPGN = searchParams.get("pgn");
     const move = searchParams.get("move");
     var turn : "white" | "black" = (searchParams.get("turn") === "black") ? "black" : "white";
     var orientation : "white" | "black" = (searchParams.get("orientation") === "black") ? "black" : "white";
 
     if (title !== null && plannedPGN !== null && (move !== null && parseInt(move) !== null)){
-        return (Flashcard(title, description, plannedPGN, parseInt(move), turn, orientation));
+        return (Flashcard(title, plannedPGN, parseInt(move), turn, orientation));
     }
     else {
       return (<Homepage />);
