c89940ae32110ac7cad1850e32338eae01c15f0d
Amir <amir.maksyutov@outlook.com>
2022-07-03 21:02:03 -0700

    New style

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -32,63 +32,64 @@ export default function Homepage() {
     }, []);
 
     return (
-        <div className="homepage-box">
-            <div className="homepage-title">Welcome to FlashChess</div>
-
-            <div className="homepage-project-description">
-                <div className="paragraph">
-                    <b>FlashChess</b> - is a platform for learning chess openings. <br />
+        <div className="homepage">
+            <div className="homepage-title">flashchess.org</div>
+            
+            <div className="homepage-box">
+                <div className="homepage-project-description">
+                    <div className="paragraph">
+                        <b>FlashChess</b> - is a platform for learning chess openings. <br />
+                    </div>
+                    <div className="paragraph">Two ways how to use it:</div>
+                    <div>1. Create opening flashcard like <a href={exampleLink}>this</a> and save its link somewhere <br />&nbsp;&nbsp;&nbsp; (e.g. in OneNote).</div>
+                    <div>2. Create your account and save your flashcard there (in developing).</div>
                 </div>
-                <div className="paragraph">Two ways how to use it:</div>
-                <div>1. Create opening flashcard like <a href={exampleLink}>this</a> and save its link somewhere <br />&nbsp;&nbsp;&nbsp; (e.g. in OneNote).</div>
-                <div>2. Create your account and save your flashcard there (in developing).</div>
+
+                <div className="homepage-form">
+                    <div className="homepage-text param">Create your own opening flashcard:</div>
+
+                    <div>
+                        <div>Title</div>
+                        <textarea className="pd-textarea title-textarea" value={title} onChange={(e) => setTitle(e.target.value)} />
+                    </div>
+
+                    <div>
+                        <div>Descritption</div>
+                        <textarea className="pd-textarea" value={description} onChange={(e) => setDescription(e.target.value)} />
+                    </div>
+
+                    <div>
+                        <div className="pgn-text">
+                            <div>PGN (</div>
+                            <div className="link">you can create it on&nbsp; <a href="https://lichess.org/analysis" target="_blank">lichess</a></div>
+                            <div>)</div>
+                        </div>
+                        <textarea className="pd-textarea" value={pgn} onChange={(e) => setPGN(e.target.value)} />
+                    </div>
+
+                    <div className="label-parts">
+                        <div>
+                            <label>Start move: <input className="move-input" type="number" value={move} onChange={(e) => setMove(parseInt(e.target.value))} /></label>
+                        </div>
+                        <div>
+                            <label>Turn: <select value={turn} onChange={(e) => setTurn(e.target.value)}>
+                                <option value="white">white</option>
+                                <option value="black">black</option>
+                            </select>
+                            </label>
+                        </div>
+                        <div>
+                            <label>Orientation: <select value={orientation} onChange={(e) => setOrientation(e.target.value)}>
+                                <option value="white">white</option>
+                                <option value="black">black</option>
+                            </select>
+                            </label>
+                        </div>
+                    </div>
+
+                    <button onClick={Create}>Create</button>
+                </div>
             </div>
-
-            <div className="homepage-form">
-                <div className="homepage-text param">Create your own opening flashcard:</div>
-
-                <div>
-                    <div>Title</div>
-                    <textarea className="pd-textarea title-textarea" value={title} onChange={(e) => setTitle(e.target.value)} />
-                </div>
-
-                <div>
-                    <div>Descritption</div>
-                    <textarea className="pd-textarea" value={description} onChange={(e) => setDescription(e.target.value)} />
-                </div>
-
-                <div>
-                    <div className="pgn-text">
-                        <div>PGN (</div>
-                        <div className="link">you can create it on&nbsp; <a href="https://lichess.org/analysis" target="_blank">lichess</a></div>
-                        <div>)</div>
-                    </div>
-                    <textarea className="pd-textarea" value={pgn} onChange={(e) => setPGN(e.target.value)} />
-                </div>
-
-                <div className="label-parts">
-                    <div>
-                        <label>Start move: <input className="move-input" type="number" value={move} onChange={(e) => setMove(parseInt(e.target.value))} /></label>
-                    </div>
-                    <div>
-                        <label>Turn: <select value={turn} onChange={(e) => setTurn(e.target.value)}>
-                            <option value="white">white</option>
-                            <option value="black">black</option>
-                        </select>
-                        </label>
-                    </div>
-                    <div>
-                        <label>Orientation: <select value={orientation} onChange={(e) => setOrientation(e.target.value)}>
-                            <option value="white">white</option>
-                            <option value="black">black</option>
-                        </select>
-                        </label>
-                    </div>
-                </div>
-
-                <button onClick={Create}>Create</button>
-            </div>
-
         </div>
     )
 }
