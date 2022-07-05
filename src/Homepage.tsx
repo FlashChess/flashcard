71afc767b4cb2dce89c7644906b98d641bd41eca
Amir <amir.maksyutov@outlook.com>
2022-07-04 23:28:39 -0700

    First step

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,7 +1,30 @@
+import "./homepage.css";
+
 export default function Homepage() {
-    return(
+    const handleClick = () => {
+        window.location.href = "https://flashchess.org/#/flashcard";
+    }
+
+    return (
         <div>
-            <h1>Homepage.tsx</h1>
+            <div className="flashchess-org">flashchess.org</div>
+
+            <div className="box">
+                <div className="description-samples-box">
+                    <div className="flashchess-description">
+
+                    </div>
+                    <div className="samples-table">
+
+                    </div>
+                </div>
+
+                <div className="description-of-work">
+
+                </div>
+
+                <button className="create-button" onClick={handleClick}>Create your own opening flashcard</button>
+            </div>
         </div>
     );
 }
